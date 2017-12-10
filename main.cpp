fdbbe7247f4cc4c16b5d36ba5fefac4f6a1e23c7
Jordan <Murrayjordan96@gmail.com>
2017-12-10 17:05:26 -0500

    Add intersection functionality to find mutual friends

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ typedef boost::graph_traits <MyGraph>::adjacency_iterator adjacency_iterator;
 typedef boost::property_map<MyGraph, boost::vertex_index_t>::type IndexMap;
 typedef boost::graph_traits<MyGraph>::vertex_iterator VertexItr;
 
+vector<int> everyone;
+
 struct connection_t{
     int id;
     int mutualFriends = 0;
@@ -52,12 +54,20 @@ void populateNetwork(MyGraph& g, ifstream& ifs) {
             second = line;
             vs = atoi(first.c_str());
             vt = atoi(second.c_str());
+            everyone.emplace_back(vs);
+            everyone.emplace_back(vt);
             boost::add_edge(vs, vt, g);
         }
         ifs.close();
     } else {
         cout << "File couldn't be opened." << endl;
     }
+
+    sort(everyone.begin(), everyone.end());
+    vector<int>::iterator it;
+    it = unique(everyone.begin(), everyone.end());
+    everyone.resize(distance(everyone.begin(), it));
+
 }
 
 vector<int> findFriends(const MyGraph& g, const int& k) {
@@ -85,8 +95,7 @@ vector<vector<int>> friendsOfFriends(const MyGraph& g, vector<int>& v) {
     return fOfF;
 }
 
-bool binarySearch(int key, const vector<int>& b)
-{
+bool binarySearch(int key, const vector<int>& b) {
     int low = 0;
     int high = b.size() - 1;
     while(high >= low)
@@ -108,8 +117,7 @@ bool binarySearch(int key, const vector<int>& b)
     return false;
 }
 
-int intersection(const vector<int>&a, const vector<int>& b)
-{
+int countIntersection(const vector<int>&a, const vector<int>& b) {
     vector<int> result;
     for (int i = 0; i < a.size(); i++)
     {
@@ -126,12 +134,29 @@ int intersection(const vector<int>&a, const vector<int>& b)
     return result.size();
 }
 
+vector<int> intersection(const vector<int>&a, const vector<int>& b) {
+    vector<int> result;
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (binarySearch(a.at(i), b))
+        {
+            result.emplace_back(a.at(i));
+        }
+    }
+
+    vector<int>::iterator it;
+    it = unique(result.begin(), result.end());
+    result.resize(distance(result.begin(), it));
+
+    return result;
+}
+
 vector<connection_t> sortedFriends(const vector<int>& KsFriends, const vector<vector<int>>& friendsFriends){
     vector<connection_t> KsNetwork;
     KsNetwork.resize(KsFriends.size());
     for (int i = 0; i < KsFriends.size(); ++i) {
         KsNetwork.at(i).id = KsFriends.at(i);
-        KsNetwork.at(i).mutualFriends = intersection(friendsFriends.at(i), KsFriends);
+        KsNetwork.at(i).mutualFriends = countIntersection(friendsFriends.at(i), KsFriends);
     }
 
     sort(KsNetwork.begin(), KsNetwork.end(), compareMutualFriends);
@@ -171,17 +196,61 @@ void topNFriends(MyGraph& g, int k, int N) {
     }
 }
 
+vector<int> notFriends(const vector<int>& friends) {
+    vector<int> result;
+    for (int i = 0; i < everyone.size(); i++)
+    {
+        if (!binarySearch(everyone.at(i), friends))
+        {
+            result.emplace_back(everyone.at(i));
+        }
+    }
+
+    vector<int>::iterator it;
+    it = unique(result.begin(), result.end());
+    result.resize(distance(result.begin(), it));
+
+    return result;
+}
+
+void TopMutualFriends(MyGraph& g, int k, int N) {
+    if (N < 0) {
+        cout << "N must be a positive integer" << endl;
+        exit(EXIT_SUCCESS);
+    }
+    vector<int> kFriends = findFriends(g, k);
+    vector<vector<int>> kFriendsOfFriends = friendsOfFriends(g, kFriends);
+    vector<int> friendNot = notFriends(kFriends);
+    friendNot.erase(find(friendNot.begin(), friendNot.end(), k));
+    vector<vector<int>> resultSet;
+	resultSet.resize(kFriendsOfFriends.size());
+
+	for(int i = 0; i < kFriendsOfFriends.size(); i++) {
+		resultSet.at(i) = intersection(kFriendsOfFriends.at(i), friendNot);
+	}
+
+    cout << "Result Set:" << endl;
+    for (int i = 0; i < resultSet.size(); i++) {
+        cout << kFriends.at(i) << ":\t";
+        for (int j = 0; j < resultSet.at(i).size(); j++) {
+            cout << resultSet.at(i).at(j) << " ";
+        }
+        cout << endl;
+    }
+
+}
+
 int main() {
-    ifstream ifs("facebook_combined.txt");
+    ifstream ifs("suggest_friends.txt");
     MyGraph g;
     populateNetwork(g, ifs);
     int k = 0;
-	int N = 20;
+	int N = 2;
 
     if (!findvertex(g, k)) {
         cout << k << " is not in the network." << endl;
         exit(EXIT_SUCCESS);
     }
-
-	topNFriends(g, k, N);
+	//topNFriends(g, k, N);
+    TopMutualFriends(g, k, N);
 }
